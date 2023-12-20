593c08930d9abcf6f0493b9f873ad5e81d38652f
STEPH <chepkiruistephanie514@gmail.com>
2023-12-20 17:20:03 +0000

    Task 2

--- console.py
+++ console.py
@@ -15,27 +15,41 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
-    # ... (previous code remains unchanged)
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
+    types = {
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
+
+    # ... (other methods and initializations remain unchanged)
 
     def do_create(self, args):
-        """Creates a new instance of a class"""
-        args = args.split()
-        if len(args) == 0:
+        """Creates a new instance of a class with given parameters"""
+        args_list = args.split()
+        if len(args_list) == 0:
             print("** class name missing **")
             return False
 
-        class_name = args[0]
+        class_name = args_list[0]
         if class_name not in self.classes:
             print("** class doesn't exist **")
             return False
 
-        args = args[1:]
-        dict_curr = self._key_value_parser(args)
-        if dict_curr is None:
+        args_list = args_list[1:]
+        params = self._key_value_parser(args_list)
+        if not params:
             print("** invalid format **")
             return False
 
-        instance = self.classes[class_name](**dict_curr)
+        instance = self.classes[class_name](**params)
         print(instance.id)
         instance.save()
 
@@ -44,10 +58,18 @@ class HBNBCommand(cmd.Cmd):
         print("Creates a class of any type")
         print("[Usage]: create <className>\n")
 
-    # Other methods go here...
-
     def _key_value_parser(self, args):
-        pass
+        """Parse key-value pairs from arguments"""
+        key_value_pairs = {}
+        for arg in args:
+            key_value = arg.split('=')
+            if len(key_value) == 2:
+                key, value = key_value
+            else:
+                print(f"Invalid argument: {arg}. Skipping...")
+        return key_value_pairs
+
+    # ... (other methods go here)
 
 
 if __name__ == "__main__":
