64f43b95c18d828ec638be30802653c2b8429ba0
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-01-16 12:15:11 -0800

    Task 5

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,83 @@
+# Puppet manifest to set up web servers for the deployment of web_static
+# Update package list and install Nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create necessary directories
+file { '/data':
+  ensure  => 'directory',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Holberton School</p>
+  </body>
+</html>",
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+}
+
+# Change ownership
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+}
+
+# Create directories for /var/www
+file { '/var/www':
+  ensure => 'directory',
+}
+
+file { '/var/www/html':
+  ensure => 'directory',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Holberton School</p>
+  </body>
+</html>",
+}
+
+# Update Nginx configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'present',
+  content => template('path/to/nginx_config.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Restart Nginx service
+service { 'nginx':
+  ensure    => running,
+  subscribe => File['/etc/nginx/sites-enabled/default'],
+}
